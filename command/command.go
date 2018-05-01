e9764321fba1d752a89edf588a01ccf50d59c736
Brian Hechinger <wonko@4amlunch.net>
2018-05-01 10:44:58 -0400

    Everything except auth-srv builds clean now. Need to figure out the NewPublication replacement.

--- command/command.go
+++ command/command.go
@@ -12,8 +12,8 @@ import (
 )
 
 type ClientFactory interface {
-	NewPermsClient() permsrv.PermissionsClient
-	NewRoleClient() rolesrv.RolesClient
+	NewPermsClient() permsrv.PermissionsService
+	NewRoleClient() rolesrv.RolesService
 }
 
 var role rclient.Roles
